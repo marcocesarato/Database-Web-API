f58298d8e2f955bdbf912cf19b083e3927c4cb3b
Marco Cesarato <marco.cesarato@eniac.it>
2020-01-29 15:58:35 +0100

    feat: new api log reader reformatted

--- logs.php
+++ logs.php
@@ -5,18 +5,18 @@ session_start();
 // TODO - Connection to database or something like
 // TODO - CHANGE IT
 $userinfo = array(
-	'user1' => '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e', // password1
-	'user2' => '6cf615d5bcaac778352a8f1f3360d23f02f34ec182e259897fd6ce485d7870d4', // password2
+    'user1' => '0b14d501a594442a01c6859541bcb3e8164d183d32937b851835442f69d5c94e', // password1
+    'user2' => '6cf615d5bcaac778352a8f1f3360d23f02f34ec182e259897fd6ce485d7870d4', // password2
 );
 
 if (isset($_POST['username'])) {
-	$_POST['password'] = hash('sha256', $_POST['password']);
-	if (@$userinfo[$_POST['username']] == $_POST['password']) {
-		$_SESSION['username'] = $_POST['username'];
-	}
+    $_POST['password'] = hash('sha256', $_POST['password']);
+    if (@$userinfo[$_POST['username']] == $_POST['password']) {
+        $_SESSION['username'] = $_POST['username'];
+    }
 }
 if (!isset($_SESSION['username'])) {
-	echo <<<EOD
+    echo <<<EOD
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -91,181 +91,181 @@ echo <<<EOD
 EOD;
 
 if (empty($_REQUEST['file'])) {
-	$logs = scandir('logs');
+    $logs = scandir('logs');
 
-	echo '<h3 class="mb-4">Logs list</h3>';
+    echo '<h3 class="mb-4">Logs list</h3>';
 
-	echo '<ul class="list-group">';
-	foreach ($logs as $log) {
-		if (strrpos($log, '.log') !== false) {
-			echo '<li class="list-group-item"><a href="?file=' . $log . '">' . $log . ' (' . (date('l', strtotime(str_replace(array('log-', '.log'), '', $log)))) . ')</a></li>';
-		}
-	}
-	echo '</ul>';
+    echo '<ul class="list-group">';
+    foreach ($logs as $log) {
+        if (strrpos($log, '.log') !== false) {
+            echo '<li class="list-group-item"><a href="?file=' . $log . '">' . $log . ' (' . (date('l', strtotime(str_replace(array('log-', '.log'), '', $log)))) . ')</a></li>';
+        }
+    }
+    echo '</ul>';
 } else {
-	$_REQUEST['file'] = str_replace(array('/', '..'), '', $_REQUEST['file']);
+    $_REQUEST['file'] = str_replace(array('/', '..'), '', $_REQUEST['file']);
 
-	if (file_exists('logs/' . $_REQUEST['file'])) {
-		include_once '../../config.php';
-		$db = new PDO('pgsql:dbname=' . $dbconfig['db_name'] . ';port=' . $dbconfig['db_port'] . ';host=' . $dbconfig['db_host_name'], $dbconfig['db_user_name'], $dbconfig['db_password']);
+    if (file_exists('logs/' . $_REQUEST['file'])) {
+        include_once '../../config.php';
+        $db = new PDO('pgsql:dbname=' . $dbconfig['db_name'] . ';port=' . $dbconfig['db_port'] . ';host=' . $dbconfig['db_host_name'], $dbconfig['db_user_name'], $dbconfig['db_password']);
 
-		$f = file_get_contents('logs/' . $_REQUEST['file']);
+        $f = file_get_contents('logs/' . $_REQUEST['file']);
 
-		$type = 'new';
-		if (strpos($f, "\n\n") !== false) {
-			$f = str_replace(array("\n\n", "\n", "\n\n"), array('[br]', '', "\n"), $f);
-			$type = 'old';
-		}
+        $type = 'new';
+        if (strpos($f, "\n\n") !== false) {
+            $f = str_replace(array("\n\n", "\n", "\n\n"), array('[br]', '', "\n"), $f);
+            $type = 'old';
+        }
 
-		$re = '/^\[(.*?)\]\s\[(.*?)\]\s\[(?:method\s)?(.*?)\]\s\[(?:url\s)?(.*?)\](?:\s\[token\s(.*?)\])?(?:\s\[client\s(.*?)\])?:(?:[\s\t])(?:([a-zA-Z]+)(\s.*))?(.*)$/m';
+        $re = '/^\[(.*?)\]\s\[(.*?)\]\s\[(?:method\s)?(.*?)\]\s\[(?:url\s)?(.*?)\](?:\s\[token\s(.*?)\])?(?:\s\[client\s(.*?)\])?:(?:[\s\t])(?:([a-zA-Z]+)(\s.*))?(.*)$/m';
 
-		preg_match_all($re, $f, $matches, PREG_SET_ORDER, 0);
+        preg_match_all($re, $f, $matches, PREG_SET_ORDER, 0);
 
-		$logs = array();
-		$users = array();
-		$log_types = array();
-		$actions = array();
-		foreach ($matches as $row) {
-			list($full_row, $date, $log_type, $method, $url, $token, $ip, $action, $query, $message) = $row;
+        $logs = array();
+        $users = array();
+        $log_types = array();
+        $actions = array();
+        foreach ($matches as $row) {
+            list($full_row, $date, $log_type, $method, $url, $token, $ip, $action, $query, $message) = $row;
 
-			if (!isset($logs[$token])) {
-				$logs[$token] = array();
+            if (!isset($logs[$token])) {
+                $logs[$token] = array();
 
-				if (!empty($token)) {
-					$sth = $db->prepare("SELECT u.first_name || ' ' || u.last_name FROM api_authentication AS a INNER JOIN users AS u ON u.id = a.user_id WHERE a.token = :token");
-					$sth->bindValue(':token', $token, PDO::PARAM_STR);
-					if ($sth->execute()) {
-						$name = $sth->fetch(PDO::FETCH_COLUMN);
-					} else {
-						$name = $token;
-					}
+                if (!empty($token)) {
+                    $sth = $db->prepare("SELECT u.first_name || ' ' || u.last_name FROM api_authentication AS a INNER JOIN users AS u ON u.id = a.user_id WHERE a.token = :token");
+                    $sth->bindValue(':token', $token, PDO::PARAM_STR);
+                    if ($sth->execute()) {
+                        $name = $sth->fetch(PDO::FETCH_COLUMN);
+                    } else {
+                        $name = $token;
+                    }
 
-					if (!isset($users[$name])) {
-						$users[$name] = array();
-					}
-					$users[$name][] = $token;
-				} elseif (!isset($users['Vuoto'])) {
-					$users['Vuoto'] = array('-empty-');
-				}
-			}
-			if (!isset($logs[$token][$log_type])) {
-				$logs[$token][$log_type] = array();
-				if (!in_array($log_type, $log_types)) {
-					$log_types[] = $log_type;
-				}
-			}
+                    if (!isset($users[$name])) {
+                        $users[$name] = array();
+                    }
+                    $users[$name][] = $token;
+                } elseif (!isset($users['Vuoto'])) {
+                    $users['Vuoto'] = array('-empty-');
+                }
+            }
+            if (!isset($logs[$token][$log_type])) {
+                $logs[$token][$log_type] = array();
+                if (!in_array($log_type, $log_types)) {
+                    $log_types[] = $log_type;
+                }
+            }
 
-			$action_group = (empty($action) ? 'MESSAGE' : $action);
-			if (!isset($logs[$token][$log_type][$action_group])) {
-				$logs[$token][$log_type][$action_group] = array();
-				if (!in_array($action_group, $actions)) {
-					$actions[] = $action_group;
-				}
-			}
+            $action_group = (empty($action) ? 'MESSAGE' : $action);
+            if (!isset($logs[$token][$log_type][$action_group])) {
+                $logs[$token][$log_type][$action_group] = array();
+                if (!in_array($action_group, $actions)) {
+                    $actions[] = $action_group;
+                }
+            }
 
-			$log = array(
-				'date'   => $date,
-				'url'    => $url,
-				'method' => $method,
-			);
+            $log = array(
+                'date' => $date,
+                'url' => $url,
+                'method' => $method,
+            );
 
-			if (!empty($action)) {
-				$log['query'] = $action . $query;
-			} else {
-				$log['message'] = $message;
-			}
+            if (!empty($action)) {
+                $log['query'] = $action . $query;
+            } else {
+                $log['message'] = $message;
+            }
 
-			$logs[$token][$log_type][$action_group][] = $log;
-		}
+            $logs[$token][$log_type][$action_group][] = $log;
+        }
 
-		echo '<h3>Select log to view</h3>';
-		echo '<form method="POST">';
-		echo '<div class="d-inline mr-2">';
-		echo '<label for="user" >User: <select class="selectpicker form-control" name="user"><option value="">Tutti</option>';
-		foreach ($users as $user => $token) {
-			$token = empty($token) ? '-empty-' : implode(',', $token);
-			echo '<option value="' . $token . '"' . (isset($_REQUEST['user']) && $_REQUEST['user'] == $token ? ' selected' : '') . '>' . $user . '</option>';
-		}
-		echo '</select></label></div>';
+        echo '<h3>Select log to view</h3>';
+        echo '<form method="POST">';
+        echo '<div class="d-inline mr-2">';
+        echo '<label for="user" >User: <select class="selectpicker form-control" name="user"><option value="">Tutti</option>';
+        foreach ($users as $user => $token) {
+            $token = empty($token) ? '-empty-' : implode(',', $token);
+            echo '<option value="' . $token . '"' . (isset($_REQUEST['user']) && $_REQUEST['user'] == $token ? ' selected' : '') . '>' . $user . '</option>';
+        }
+        echo '</select></label></div>';
 
-		echo '<div class="d-inline mr-2">';
-		echo '<label for="type">Log type: <select class="selectpicker form-control" name="type"><option value="">Tutti</option>';
-		foreach ($log_types as $log_type) {
-			echo '<option' . (isset($_REQUEST['type']) && $_REQUEST['type'] == $log_type ? ' selected' : '') . '>' . $log_type . '</option>';
-		}
-		echo '</select></label></div>';
+        echo '<div class="d-inline mr-2">';
+        echo '<label for="type">Log type: <select class="selectpicker form-control" name="type"><option value="">Tutti</option>';
+        foreach ($log_types as $log_type) {
+            echo '<option' . (isset($_REQUEST['type']) && $_REQUEST['type'] == $log_type ? ' selected' : '') . '>' . $log_type . '</option>';
+        }
+        echo '</select></label></div>';
 
-		echo '<div class="d-inline mr-2">';
-		echo '<label for="action">Action: <select class="selectpicker form-control" name="action"><option value="">Tutte</option>';
-		foreach ($actions as $action) {
-			echo '<option' . (isset($_REQUEST['action']) && $_REQUEST['action'] == $action ? ' selected' : '') . '>' . $action . '</option>';
-		}
-		echo '</select></label></div>';
+        echo '<div class="d-inline mr-2">';
+        echo '<label for="action">Action: <select class="selectpicker form-control" name="action"><option value="">Tutte</option>';
+        foreach ($actions as $action) {
+            echo '<option' . (isset($_REQUEST['action']) && $_REQUEST['action'] == $action ? ' selected' : '') . '>' . $action . '</option>';
+        }
+        echo '</select></label></div>';
 
-		echo '<div class="d-inline">';
-		echo '<input class="btn btn-primary" type="submit" value="Visualizza" name="show" />';
-		echo '</div>';
-		echo '</form>';
+        echo '<div class="d-inline">';
+        echo '<input class="btn btn-primary" type="submit" value="Visualizza" name="show" />';
+        echo '</div>';
+        echo '</form>';
 
-		if (isset($_REQUEST['show'])) {
-			$user = explode(',', $_REQUEST['user']);
-			$log_type = $_REQUEST['type'];
-			$action = $_REQUEST['action'];
+        if (isset($_REQUEST['show'])) {
+            $user = explode(',', $_REQUEST['user']);
+            $log_type = $_REQUEST['type'];
+            $action = $_REQUEST['action'];
 
-			$users_new = array();
-			foreach ($users as $username => $u) {
-				foreach ($u as $t) {
-					$users_new[$t] = $username;
-				}
-			}
-			$users = $users_new;
+            $users_new = array();
+            foreach ($users as $username => $u) {
+                foreach ($u as $t) {
+                    $users_new[$t] = $username;
+                }
+            }
+            $users = $users_new;
 
-			$users_db = array();
-			$sth = $db->prepare("SELECT id, first_name || ' ' || last_name AS name FROM users");
-			if ($sth->execute()) {
-				$users_db = $sth->fetchAll(PDO::FETCH_ASSOC);
-			}
+            $users_db = array();
+            $sth = $db->prepare("SELECT id, first_name || ' ' || last_name AS name FROM users");
+            if ($sth->execute()) {
+                $users_db = $sth->fetchAll(PDO::FETCH_ASSOC);
+            }
 
-			$output = array();
-			foreach ($logs as $t => $log) {
-				$t = empty($t) ? '-empty-' : $t;
-				if (empty($user[0]) || in_array($t, $user)) {
-					foreach ($log as $lt => $types) {
-						if (empty($log_type) || ($lt === $log_type)) {
-							foreach ($types as $a => $actions) {
-								if (empty($action) || ($a === $action)) {
-									$u = (isset($users[$t]) ? $users[$t] : '');
+            $output = array();
+            foreach ($logs as $t => $log) {
+                $t = empty($t) ? '-empty-' : $t;
+                if (empty($user[0]) || in_array($t, $user)) {
+                    foreach ($log as $lt => $types) {
+                        if (empty($log_type) || ($lt === $log_type)) {
+                            foreach ($types as $a => $actions) {
+                                if (empty($action) || ($a === $action)) {
+                                    $u = (isset($users[$t]) ? $users[$t] : '');
 
-									foreach ($actions as $def) {
-										if (isset($def['message']) && in_array(substr($def['message'], 0, 1), array('{', '['))) {
-											$msg = json_decode($def['message'], true);
+                                    foreach ($actions as $def) {
+                                        if (isset($def['message']) && in_array(substr($def['message'], 0, 1), array('{', '['))) {
+                                            $msg = json_decode($def['message'], true);
 
-											if (isset($msg['user'])) {
-												$key = array_search($msg['user']['id'], array_column($users_db, 'id'));
-												if (!empty($key)) {
-													$u = $users_db[$key]['name'];
-												}
-											}
-										}
+                                            if (isset($msg['user'])) {
+                                                $key = array_search($msg['user']['id'], array_column($users_db, 'id'));
+                                                if (!empty($key)) {
+                                                    $u = $users_db[$key]['name'];
+                                                }
+                                            }
+                                        }
 
-										$output_date = strtotime($def['date']);
-										while (isset($output[$output_date])) {
-											$output_date++;
-										}
-										$output[$output_date] = '<tr class="' . strtolower($lt) . '"><td>' . $def['date'] . '</td><td>' . $u . '</td>' . (empty($log_type) ? '<td>' . $lt . '</td>' : '') . '<td>' . (isset($def['query']) ? $def['query'] : $def['message']) . '</td><td>' . $def['method'] . '</td><td>' . $def['url'] . '</td></tr>';
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			echo '<h4>Results</h4>';
-			echo '<table class="table" id="logs-table"><thead><tr><th scope="col">Date</th><th scope="col">User</th>' . (empty($log_type) ? '<th scope="col">Type</th>' : '') . '<th scope="col">Query / Message</th><th scope="col">Method</th><th scope="col">URL</th></tr></thead><tbody>';
-			ksort($output, SORT_NUMERIC);
-			echo implode('', $output);
-			echo '</tbody></table>';
-			echo '<style>
+                                        $output_date = strtotime($def['date']);
+                                        while (isset($output[$output_date])) {
+                                            $output_date++;
+                                        }
+                                        $output[$output_date] = '<tr class="' . strtolower($lt) . '"><td>' . $def['date'] . '</td><td>' . $u . '</td>' . (empty($log_type) ? '<td>' . $lt . '</td>' : '') . '<td>' . (isset($def['query']) ? $def['query'] : $def['message']) . '</td><td>' . $def['method'] . '</td><td>' . $def['url'] . '</td></tr>';
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            echo '<h4>Results</h4>';
+            echo '<table class="table" id="logs-table"><thead><tr><th scope="col">Date</th><th scope="col">User</th>' . (empty($log_type) ? '<th scope="col">Type</th>' : '') . '<th scope="col">Query / Message</th><th scope="col">Method</th><th scope="col">URL</th></tr></thead><tbody>';
+            ksort($output, SORT_NUMERIC);
+            echo implode('', $output);
+            echo '</tbody></table>';
+            echo '<style>
                 table th{ background-color: #333; color: #fff; font-weight: bold; }
                 table td{ border: 0; }
                 table tr:not(:last-child) td{ border-bottom: 1px solid gray; }
@@ -277,10 +277,10 @@ if (empty($_REQUEST['file'])) {
                 table tr.error:nth-child(odd) td{ background-color: #f3d4d4; }
                 table tr.error:nth-child(even) td{ background-color: #f3bdbe; }
             </style>';
-		}
-	} else {
-		echo 'File not found.';
-	}
+        }
+    } else {
+        echo 'File not found.';
+    }
 }
 
 echo <<<EOD
